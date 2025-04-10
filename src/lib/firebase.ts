a3abf341e608334bad7956604f8ff1494b99278b
marzy-142 <canetemariella888@gmail.com>
2025-04-10 11:29:22 +0800

    another

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth } from 'firebase/auth';
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDGCdsHSjB3PrGo9pUkdPbgXV9-zMOeUI8",
-  authDomain: "bloodlink-demo.firebaseapp.com",
-  projectId: "bloodlink-demo",
-  storageBucket: "bloodlink-demo.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:a1b2c3d4e5f6a7b8c9d0e1"
+  apiKey: "AIzaSyAF-fjHh9B88OHoHjbCyMgc5ktLaW9EwNA",
+  authDomain: "bloodlink-13d2b.firebaseapp.com",
+  projectId: "bloodlink-13d2b",
+  storageBucket: "bloodlink-13d2b.firebasestorage.app",
+  messagingSenderId: "651543224130",
+  appId: "1:651543224130:web:8fed1ab0672afe9c5032db"
 };
 
 // Initialize Firebase
